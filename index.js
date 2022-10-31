bd5de51719d5a23450c519c7a8150b7f0b0a7498
Mujeeb4582 <mujeeburahman4582@gmail.com>
2022-10-31 18:46:01 +0500

    Remove the object and add to the separate module

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 import {
-  addBookForm, list, msg, success,
+  addBookForm, msg, success,
 } from './modules/HtmlElements.js';
 import { clearField } from './modules/clearInputField.js';
 import navMenu from './modules/nav-menu.js';
 
+// eslint-disable-next-line import/no-cycle
+import BookInfo from './modules/bookInfo.js';
+
+// eslint-disable-next-line import/no-mutable-exports
 let booksList = []; // Declare a array for storing the bookList
 
 function ondate() {
@@ -23,48 +27,7 @@ const emptyMessage = () => {
 // nav section
 navMenu();
 
-// make object instance and its methods
-class BookInfo {
-  // create the object using constructor method
-  constructor(title, author) {
-    this.title = title;
-    this.author = author;
-  }
-
-  createListItem(booksListIndex) {
-    const listWrapper = document.createElement('div');
-    listWrapper.className = 'listWrapper';
-    const unOrderList = document.createElement('ul');
-    const li1 = document.createElement('li');
-    const li2 = document.createElement('li');
-    const li3 = document.createElement('li');
-    const removeBtn = document.createElement('button');
-    removeBtn.innerHTML = 'Remove';
-    removeBtn.type = 'button';
-
-    list.appendChild(listWrapper);
-    listWrapper.appendChild(unOrderList);
-    // set innerHTML value
-    li1.innerHTML = `'${this.title}'`;
-    li2.innerHTML = this.author;
-    li3.innerHTML = 'by';
-    unOrderList.appendChild(li1);
-    unOrderList.appendChild(li3);
-    unOrderList.appendChild(li2);
-    listWrapper.appendChild(removeBtn);
-
-    // remove button click listner
-    removeBtn.addEventListener('click', (event) => {
-    // Remove item from DOM
-      event.target.parentElement.remove();
-      // Remove item from booksList Array
-      booksList.splice(booksListIndex, 1);
-      // Remove item from localStorage
-      localStorage.setItem('list', JSON.stringify(booksList));
-      emptyMessage();
-    });
-  }
-}
+export { emptyMessage, booksList };
 
 // Loop over Array, create the element and append to the DOM
 function renderBookList(lists) {
