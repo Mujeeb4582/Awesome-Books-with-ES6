252e250e4d563de112f2a041cb75d8a523888eed
Mujeeb4582 <mujeeburahman4582@gmail.com>
2023-01-13 17:49:19 +0500

    Remove the linter error

--- modules/clearInputField.js
+++ modules/clearInputField.js
@@ -1,7 +1,8 @@
 import { addBookForm } from './HtmlElements.js';
 
-// eslint-disable-next-line import/prefer-default-export
-export const clearField = () => {
+const clearField = () => {
   addBookForm.bookTitle.value = '';
   addBookForm.bookAuthor.value = '';
 };
+
+export default clearField;
