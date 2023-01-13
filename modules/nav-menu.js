e5ee195f3fa1e6e7268dd00b034e65ffdef69a8b
Mujeeb4582 <mujeeburahman4582@gmail.com>
2023-01-13 17:25:03 +0500

    Refactor the code

--- modules/nav-menu.js
+++ modules/nav-menu.js
@@ -1,61 +1,61 @@
 // nav section start here
 import {
-  // eslint-disable-next-line max-len
-  listSection, addSection, contactSection, books, heading, add, addTextColor, listTextColor, contactTextColor,
+  listSection,
+  addSection,
+  contactSection,
+  books,
+  heading,
+  addTextColor,
+  listTextColor,
+  contactTextColor,
 } from './HtmlElements.js';
 
+const navItemsColor = (addColor, listColor, contactColor) => {
+  addTextColor.style.color = addColor;
+  listTextColor.style.color = listColor;
+  contactTextColor.style.color = contactColor;
+};
+
+const showSection = (bookList, addBook, contact, bookSection, title) => {
+  listSection.setAttribute('class', bookList);
+  addSection.setAttribute('class', addBook);
+  contactSection.setAttribute('class', contact);
+  books.setAttribute('class', bookSection);
+  heading.setAttribute('class', title);
+};
 const navMenu = () => {
   // Show add section on website when website loaded
   const showAddSectionFirst = () => {
-    listSection.setAttribute('class', 'non-show');
-    addSection.setAttribute('class', 'show');
-    contactSection.setAttribute('class', 'non-show');
-    books.setAttribute('class', 'books');
-    heading.setAttribute('class', 'non-show');
-    add.style.color = 'green';
+    showSection('non-show', 'show', 'non-show', 'books', 'non-show');
+    navItemsColor('green', 'black', 'black');
   };
 
   showAddSectionFirst();
-
   const showList = () => {
-    listSection.setAttribute('class', 'show');
-    addSection.setAttribute('class', 'non-show');
-    contactSection.setAttribute('class', 'non-show');
-    books.setAttribute('class', 'books');
-    heading.setAttribute('class', 'heading');
-    addTextColor.style.color = 'black';
-    listTextColor.style.color = 'green';
-    contactTextColor.style.color = 'black';
+    showSection('show', 'non-show', 'non-show', 'books', 'heading');
+    navItemsColor('black', 'green', 'black');
   };
 
   const showAdd = () => {
-    addTextColor.style.color = 'green';
-    listTextColor.style.color = 'black';
-    contactTextColor.style.color = 'black';
-    listSection.setAttribute('class', 'non-show');
-    addSection.setAttribute('class', 'show');
-    contactSection.setAttribute('class', 'non-show');
-    books.setAttribute('class', 'books');
-    heading.setAttribute('class', 'non-show');
+    showSection('non-show', 'show', 'non-show', 'books', 'non-show');
+    navItemsColor('green', 'black', 'black');
   };
 
-  const showcontact = () => {
-    books.setAttribute('class', 'non-show');
-    heading.setAttribute('class', 'non-show');
-    contactSection.setAttribute('class', 'show');
-    addTextColor.style.color = 'black';
-    listTextColor.style.color = 'black';
-    contactTextColor.style.color = 'green';
+  const showContact = () => {
+    showSection('non-show', 'non-show', 'show', 'non-show', 'non-show');
+    navItemsColor('black', 'black', 'green');
   };
 
   addTextColor.addEventListener('click', () => {
     showAdd();
   });
+
   listTextColor.addEventListener('click', () => {
     showList();
   });
+
   contactTextColor.addEventListener('click', () => {
-    showcontact();
+    showContact();
   });
 };
 // nav section ends here
